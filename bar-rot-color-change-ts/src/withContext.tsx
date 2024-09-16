fee6950b2fc9434cdb18ddfbc08858a5a7b6379a
Anwesh43 <anweshthecool0@gmail.com>
2024-09-16 09:07:42 +0530

    bar-rot-color-change: adding withContext HOC

---
+++ bar-rot-color-change-ts/src/withContext.tsx
@@ -0,0 +1,19 @@
+
+import * as React from "react";
+import { useAnimatedScale, useDimension } from "./hooks";
+
+const withContext = (MainComponent : React.FC<any>) => {
+    return (props : any) => {
+        const {w, h} = useDimension()
+        const {scale, start : onClick, i} = useAnimatedScale()
+        const newProps = {
+            ...props,
+            w,
+            h, 
+            scale, 
+            i,
+            onClick 
+        }
+        return <MainComponent {...newProps}></MainComponent>
+    }
+} 
