ba52e6f6ad80a2cceb00d4544ce6c14f433f1cb0
Anwesh43 <anweshthecool0@gmail.com>
2024-09-16 09:01:24 +0530

    bar-rot-color-change: adding useDimension hook

--- bar-rot-color-change-ts/src/hooks.ts
+++ bar-rot-color-change-ts/src/hooks.ts
@@ -1,6 +1,12 @@
 import {useState, useEffect} from 'react'
 
-
+const colors : Array<string> = [
+    "#1A237E",
+    "#EF5350",
+    "#AA00FF",
+    "#C51162",
+    "#00C853"
+]
 export const useAnimatedScale = (scGap : number = 0.01, delay : number = 20) => {
     const [scale, setScale] = useState<number>(0)
     const [animated, setAnimated] = useState<Boolean>(false)
@@ -16,6 +22,7 @@ export const useAnimatedScale = (scGap : number = 0.01, delay : number = 20) => 
                         if (prev > 1) {
                             setAnimated(false)
                             clearInterval(interval)
+                            setI(i % colors.length)
                             return 0
                         }
                         return prev + scGap 
@@ -23,5 +30,24 @@ export const useAnimatedScale = (scGap : number = 0.01, delay : number = 20) => 
                 }, delay)
             }
         }
+    }
+}
+
+export const useDimension = () => {
+    const [w, setW] = useState(window.innerWidth)
+    const [h, setH] = useState(window.innerHeight)
+    useEffect(() => {
+        const resizeListener = () => {
+            setW(window.innerWidth)
+            setH(window.innerHeight)
+        }
+        window.addEventListener('resize', resizeListener, false)
+        return () => {
+            window.removeEventListener('resize', resizeListener)
+        }
+    })
+    return {
+        w, 
+        h, 
     }
 }
